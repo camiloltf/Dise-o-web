8072295df0ab60eb55e9d1c06c212dc07ef6ae30
camiloltf <camiloltf99@gmail.com>
2026-04-17 11:11:16 -0500

    Add files via upload

---
+++ CLASE9/sketch.js
@@ -0,0 +1,49 @@
+function setup() {
+    createCanvas(400, 300);
+    frameRate(30);
+    colorMode(HSB);
+    noStroke();
+
+}
+
+function draw() {
+    background(220);
+
+    fill('#8b3333ff');
+    rect(0, 0, 400, 300);
+
+    fill('#3b3b3bff');
+    rect(150, 120, 100, 180);
+
+    fill('#5e887aff');
+    rect(140, 120, 120, 15);
+
+    fill('#000000ff');
+    rect(195, 60, 5, 60);
+
+    fill('#a5c0c9ff');
+    rect(165, 250, 30, 10);
+
+    fill('#a5c0c9ff');
+    rect(165, 220, 30, 10);
+
+    fill('#a5c0c9ff');
+    rect(165, 190, 30, 10);
+
+    fill('#a5c0c9ff');
+    rect(165, 160, 30, 10);
+
+    fill('#a5c0c9ff');
+    rect(205, 250, 30, 10);
+
+    fill('#a5c0c9ff');
+    rect(205, 220, 30, 10);
+
+    fill('#a5c0c9ff');
+    rect(205, 190, 30, 10);
+
+    fill('#a5c0c9ff');
+    rect(205, 160, 30, 10);
+}
+
+
